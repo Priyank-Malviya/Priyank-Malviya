007b32b83c5ff505ea9d82c7a0f5c792a62fc3e3
Priyank Malviya <priyankmalviy@gmail.com>
2023-07-15 14:18:51 +0530

    code

--- server.py
+++ server.py
@@ -15,62 +15,69 @@ clients = {}
 
 
 def broadcast(message):
-        for client in clients.values():
-                client.send(message)
+    for client in clients.values():
+        client.send(message)
+
 
 def send_message(client, message):
     print(message)
     client.send(message)
 
+
 def handle_connection(client):
-        stop = False
-        while not stop:
-                try:
-                        message= client.recv(1024)
-                        broadcast(message)
-                except:
-                        nickname = None
-                        addr = None
-                        for key, value in clients.items():
-                                if value == client:
-                                        addr = key
-                                        break
-                        for key, value in nicknames.items():
-                                if value == addr:
-                                        nickname = key
-                                        break
-                        if nickname and addr:
-                                del nicknames[nickname]
-                                del clients[addr]
-                        
-                                broadcast(f"{nickname} left the chat!".encode('utf-8'))
-                                stop = True
+    stop = False
+    while not stop:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            nickname = None
+            addr = None
+            for key, value in clients.items():
+                if value == client:
+                    addr = key
+                    break
+            for key, value in nicknames.items():
+                if value == addr:
+                    nickname = key
+                    break
+            if nickname and addr:
+                del nicknames[nickname]
+                del clients[addr]
+
+                broadcast(f"{nickname} left the chat!".encode('utf-8'))
+                stop = True
+
 
 def main():
-        print('server is running...')
+    print('Server is running...')
 
-        while True:
-                try:
-                        client, addr = server.accept()
-                except:
-                        print("already connected!!")
-                print(f"Connected to {addr}")
-                
-                nickname = client.recv(1024).decode('utf-8')
-                if nickname not in nicknames:
-                        res = "1"
-                        nicknames[nickname] = addr
-                        clients[addr] = client                        
-                        print(f"NickName is {nickname}")
-                        client.send(f"{res}".encode('utf-8'))
-                        
-                        nicknames_json = json.dumps(nicknames)
-                        send_message(client, nicknames_json.encode('utf-8'))
+    while True:
+        try:
+            client, addr = server.accept()
+        except:
+            print("Already connected!!")
+            continue
+        print(f"Connected to {addr}")
 
-                        thread = threading.Thread(target=handle_connection, args=(client,))
-                        thread.start()
-                else:
-                        client.send("0".encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+        if nickname not in nicknames:
+            res = "1"
+            nicknames[nickname] = addr
+            clients[addr] = client
+            print(f"Nickname is {nickname}")
+            client.send(f"{res}".encode('utf-8'))
+
+            nicknames_json = json.dumps(nicknames)
+            send_message(client, nicknames_json.encode('utf-8'))
+
+            thread = threading.Thread(target=handle_connection, args=(client,))
+            thread.start()
+        else:
+            res = "0"
+            client.send(f"{res}".encode('utf-8'))
+            client.close()
+
 
 if __name__ == '__main__':
-        main() 
+    main()
